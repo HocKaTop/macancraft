8ee541fb5a602d79d2d2187bd2558de4ae1237ac
HocKaTop <oleg.slobodeniuc@iis.utm.md>
2026-02-07 23:48:06 +0200

    fixed macan entity

--- src/main/java/macancraft/entity/client/MacanRenderer.java
+++ src/main/java/macancraft/entity/client/MacanRenderer.java
@@ -3,16 +3,19 @@ package macancraft.entity.client;
 import macancraft.Macancraft;
 import macancraft.entity.MacanEntity;
 import net.minecraft.client.render.entity.*;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.util.Identifier;
 
-public class MacanRenderer extends MobEntityRenderer<MacanEntity, MacanModel<MacanEntity>> {
-    private static final Identifier TEXTURE =
-            Identifier.of(Macancraft.MOD_ID, "textures/entity/macan.png");
+public class MacanRenderer extends LivingEntityRenderer<MacanEntity, PlayerEntityModel<MacanEntity>> {
 
-    public MacanRenderer(EntityRendererFactory.Context context) {
+    private static final Identifier TEXTURE =
+            Identifier.of("macancraft", "textures/entity/macan.png");
+
+    public MacanRenderer(EntityRendererFactory.Context ctx) {
         super(
-                context,
-                new MacanModel<>(context.getPart(ModModelLayers.MACAN)),
+                ctx,
+                new PlayerEntityModel<>(ctx.getPart(EntityModelLayers.PLAYER), false),
                 0.5f
         );
     }
